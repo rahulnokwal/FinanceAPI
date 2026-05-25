50cdc6c597d4c004347b431be0b2dccfb440079d
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-05-25 21:04:05 +0530

    feat: add Cloudinary integration for profile image uploads and create multer middleware for file handling

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadProfile = async (filepath) => {
+  try {
+    if (!filepath) return null;
+    const fileResponse = await cloudinary.uploader.upload(filepath, {
+      resource_type: "auto",
+    });
+    console.log("file uploaded successfully on cloudinary");
+    if (fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath);
+    }
+  } catch (error) {
+    if (fs.existsSync(filepath)) {
+      fs.unlinkSync(filepath);
+    }
+    console.error("file upload failed", error);
+    return null;
+  }
+};
+
+export default uploadProfile;
