1b544377ebc386e8e76190123df108a1c0a993fa
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-05-25 18:58:06 +0530

    feat: add Expense and User models for expense tracking functionality

---
+++ src/models/Expense.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const expenseSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    amount: {
+      type: mongoose.Types.Decimal128,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+export const Expense = mongoose.model("Expense", expenseSchema);
---
+++ src/models/User.models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    profile: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+export const User = mongoose.model("User", userSchema);
